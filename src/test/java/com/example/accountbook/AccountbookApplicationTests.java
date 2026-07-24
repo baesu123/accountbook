57acf7647d954d14dbb060b98f0f6e3c9236ead1
bsh <qotngus72@naver.com>
2026-07-24 10:16:20 +0900

    환경설정

---
+++ src/test/java/com/example/accountbook/AccountbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountbookApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountbookApplication.class, args);
+	}
+
+}
